57d93c042ed0aea69eeb4afdccf1278bf2b72f80
JAIP1003 <jgatep@gmail.com>
2020-07-11 19:55:59 +0530

    Updates

---
+++ precache-manifest.aa218957fff21b6c6e6fb0e7d2aee9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c592d6e96d262e99e5ff5f1f93d602",
+    "url": "/myFirstWebsite/index.html"
+  },
+  {
+    "revision": "27ea3712fd57597049f1",
+    "url": "/myFirstWebsite/static/css/main.040d9d90.chunk.css"
+  },
+  {
+    "revision": "aa577bae33e1531d3f7f",
+    "url": "/myFirstWebsite/static/js/2.27726f6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myFirstWebsite/static/js/2.27726f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea3712fd57597049f1",
+    "url": "/myFirstWebsite/static/js/main.a92c9082.chunk.js"
+  },
+  {
+    "revision": "c7594c1d5bf88ca974e5",
+    "url": "/myFirstWebsite/static/js/runtime-main.70f16557.js"
+  }
+]);
